b45ae11aed0b3ef53126f393cede224cbf7ca48d
ryandaaa <ryanda.valents3649@student.unri.ac.id>
2026-05-27 13:02:59 +0700

    fix splash n flickering

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,7 @@
+// Prevents a CMD/console window from appearing on Windows when launching the app.
+// This attribute is ignored on macOS and Linux.
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 fn main() {
     devwannawave_lib::run();
 }
